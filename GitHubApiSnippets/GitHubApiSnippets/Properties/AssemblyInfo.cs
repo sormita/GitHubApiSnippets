d287a4b3839b8ae3f2b8aa58f6af0dabc4b9c11c
sormitaibm <sormitach@in.ibm.com>
2022-03-24 14:14:46 +0530

    Initial commit

---
+++ GitHubApiSnippets/GitHubApiSnippets/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GitHubApiSnippets")]
+[assembly: AssemblyDescription("A simple console application to provide code snippets for use when describing the GitHub API.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Transeric Solutions")]
+[assembly: AssemblyProduct("GitHubApiSnippets")]
+[assembly: AssemblyCopyright("Copyright © 2019 Transeric Solutions. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("368f3c35-4ec3-4da7-80e9-1c94b9d8dd7a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
